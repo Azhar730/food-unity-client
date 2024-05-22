22047e72f35ab9f75e746ed59edd647ae700ca62
Azhar730 <azharmahmud730@gmail.com>
2024-05-22 14:11:36 +0600

    404 page added done

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Error = () => {
     return (
         <div>
-            <h1 className="text-[500px] text-center">404</h1>
+            <img src="https://i.postimg.cc/Fs41RDqS/404.jpg" />
             <p className="rounded-lg text-[#FFF] font-semibold text-lg mx-auto p-2 bg-[#b75757] border-2 w-fit"><Link to={'/'} className="text-center">Go Home</Link></p>
         </div>
     );
